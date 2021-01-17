65571d7b02217b48af20969b3bf7a2feb59bfd31
KiyonoOsawa <kiyo.sntr.x6@icloud.com>
2021-01-17 15:35:15 +0900

    Initial Commit

---
+++ Associative game/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Associative game
+//
+//  Created by 大澤清乃 on 2021/01/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
